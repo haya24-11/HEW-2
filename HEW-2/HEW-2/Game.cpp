27e6f4d7959c53296d9e7e4875e14ef8f220a489
NemuiRia <meriodasu0415@gmail.com>
2025-12-16 18:01:08 +0900

    背景色ついかー

--- HEW-2/HEW-2/Game.cpp
+++ HEW-2/HEW-2/Game.cpp
@@ -1,4 +1,6 @@
 #include "Game.h"
+#include "Application.h"
+#include <windows.h>
 
 //コンストラクタ
 Game::Game()
@@ -23,6 +25,18 @@ void Game::Update()
 //描画処理
 void Game::Draw()
 {
+    // ウィンドウのDC取得
+    HDC hdc = GetDC(Application::GetWindow());
+
+    // 背景を塗る（青）
+    RECT rc;
+    GetClientRect(Application::GetWindow(), &rc);
+    HBRUSH brush = CreateSolidBrush(RGB(100, 180, 255));
+    FillRect(hdc, &rc, brush);
+
+    // 後始末
+    DeleteObject(brush);
+    ReleaseDC(Application::GetWindow(), hdc);
 }
 
 
