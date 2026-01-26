156271904f9a549ad3b638ad5fcfd372aeb6a3ea
huning2000 <hachioji1001@gmail.com>
2026-01-26 13:39:55 +0900

    GamePlay修正

--- HEW-2/HEW-2/GamePlay.cpp
+++ HEW-2/HEW-2/GamePlay.cpp
@@ -1,17 +1,28 @@
 #include "GamePlay.h"
-
+#include <iostream>
 GamePlay::GamePlay():Scene(SceneType::GamePlay)
 {
 }
 
 void GamePlay::InitScene()
 {
-	std::cout << "i" << std::endl;
+	std::cout << "[GamePlay]InitScene" << std::endl;
+
+	// PLAYER OBJECT
+	Object* player = AddObject();
+	player->Init("asset/Texture/Player.png");
+	player->SetPos(0.0f, 0.0f, 0.0f);
+	player->SetSize(100.0f, 100.0f, 0.0f);
+
+	// ENEMY OBJECT
+	Object* enemy = AddObject();
+	enemy->Init("asset/Texture/Enemy.png");
+	enemy->SetPos(200.0f, 0.0f, 0.0f);
+	enemy->SetSize(100.0f, 100.0f, 0.0f);
 }
 
 void GamePlay::UpdateScene(float deltaTime)
 {
-
 	if (Input::GetKeyTrigger(VK_SPACE))
 	{
 		SetNextScene(SceneType::Result);
@@ -27,5 +38,6 @@ void GamePlay::DrawScene()
 }
 void GamePlay::UninitScene()
 {
-
+	std::cout << "[GamePlay]UninitScene" << std::endl;
 }
+
