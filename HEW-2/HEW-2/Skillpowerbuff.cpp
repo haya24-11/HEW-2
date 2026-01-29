d43a7ff23dc142974f8ee1a1e57e0cf35e69689e
haya24-11 <uj41234@gmail.com>
2026-01-30 03:51:00 +0900

    バフ

--- HEW-2/HEW-2/Skillpowerbuff.cpp
+++ HEW-2/HEW-2/Skillpowerbuff.cpp
@@ -7,7 +7,11 @@ Skillpowerbuff::Skillpowerbuff(int value):BuffSkill("Buff",4),m_buffValue(value)
 
 
 
-void Apply(Player* player)
+static void Apply(Player* player)
 {
 	if (!player)return;
+
+	int atk = player->GetPower();
+	atk = static_cast<int>(atk *0.2);
+	player->SetPower(atk);
 }
